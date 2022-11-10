a4315594567371eded6cbbffb6668156a787d74d
qrvck <v.kapitan2010@gmail.com>
2022-11-10 17:27:05 +0300

    feat: add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from 'components/common/layout/Layout';
+import { AuthPage, BoardManagementPage, BoardsListPage, WelcomePage } from 'pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="board-management" element={<BoardManagementPage />} />
+        <Route path="boards-list" element={<BoardsListPage />} />
+      </Route>
+    </Routes>
   );
 }
 
