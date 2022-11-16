9375aa39652b7e79446e4b73b68d7537c7bfdc43
qrvck <v.kapitan2010@gmail.com>
2022-11-16 19:41:04 +0300

    feat: save the translation for the boards list page to i18next

--- src/components/boardsListPage/boardCard/BoardCard.tsx
+++ src/components/boardsListPage/boardCard/BoardCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 
@@ -13,11 +15,15 @@ interface IBoardCardProps {
 }
 
 function BoardCard({ to, tittle, owner }: IBoardCardProps) {
+  const { t } = useTranslation('boards-list-page');
+
   return (
     <NavLink className={styles.link} to={to}>
       <Paper className={styles.paper} elevation={4}>
         <p className={styles.title}>{tittle}</p>
-        <p className={styles.owner}>owner: {owner}</p>
+        <p className={styles.owner}>
+          {t('card.owner')}: {owner}
+        </p>
         <IconButton className={styles.deleteBtn}>
           <DeleteIcon className={styles.deleteIcon} />
         </IconButton>
--- src/pages/BoardsListPage.tsx
+++ src/pages/BoardsListPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import BoardsList from 'components/boardsListPage/boardsList/BoardsList';
 
 function BoardsListPage() {
+  const { t } = useTranslation('boards-list-page');
+
   return (
     <>
-      <h2>Boards</h2>
+      <h2>{t('title')}</h2>
       <BoardsList />
     </>
   );
