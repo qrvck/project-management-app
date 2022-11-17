ba11a83fb8505379c8aa899310acdc3bf76c1ea4
qrvck <v.kapitan2010@gmail.com>
2022-11-17 17:01:47 +0300

    refactor: replace delete icon of board card with delete icon mui

--- src/components/boardsListPage/boardCard/BoardCard.tsx
+++ src/components/boardsListPage/boardCard/BoardCard.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 
-import { ReactComponent as DeleteIcon } from 'assets/icons/delete-icon.svg';
+import Delete from '@mui/icons-material/Delete';
 import styles from './BoardCard.module.scss';
 
 interface IBoardCardProps {
@@ -25,7 +25,7 @@ function BoardCard({ to, tittle, owner }: IBoardCardProps) {
           {t('card.owner')}: {owner}
         </p>
         <IconButton className={styles.deleteBtn}>
-          <DeleteIcon className={styles.deleteIcon} />
+          <Delete />
         </IconButton>
       </Paper>
     </NavLink>
