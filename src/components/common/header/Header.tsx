22652c65127743fe9879f614baab88bd3212fa70
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-20 14:11:33 +0300

    feat: add styles for header container and logo, make header sticky and animated

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './Header.module.scss';
 
 function Header() {
+  const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
+  const headerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const header = headerRef?.current?.getBoundingClientRect() as DOMRect;
+    const updateHeaderOnScroll = () => {
+      if (window.pageYOffset > header.top + header.height) {
+        setSticky({ isSticky: true, offset: header.height });
+      } else {
+        setSticky({ isSticky: false, offset: 0 });
+      }
+    };
+
+    window.addEventListener('scroll', updateHeaderOnScroll);
+
+    return () => {
+      window.removeEventListener('scroll', updateHeaderOnScroll);
+    };
+  }, []);
+
   return (
-    <header>
-      <div className="container">
-        <h1>Header</h1>
+    <header
+      className={sticky.isSticky ? `${styles.header} ${styles.sticky}` : styles.header}
+      ref={headerRef}
+    >
+      <div className={`container ${styles.container}`}>
+        <h1 className={styles.logo}>Project Management</h1>
       </div>
     </header>
   );
