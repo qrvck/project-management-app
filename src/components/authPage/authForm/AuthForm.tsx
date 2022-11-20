1f85bea895eb12f0806783671fa3db6733e718fe
qrvck <v.kapitan2010@gmail.com>
2022-11-20 17:46:33 +0300

    feat: do auth form responsive

--- src/components/authPage/authForm/AuthForm.tsx
+++ src/components/authPage/authForm/AuthForm.tsx
@@ -49,191 +49,193 @@ function AuthForm() {
 
   return (
     <div className={styles.wrapper}>
-      <div>
-        <img className={styles.welcomeImage} src={authGreeting} />
-        <p className={styles.imageTitle}>{t('image title')}</p>
-        <p className={styles.imageSubtitle}>{t('image subtitle')}</p>
-      </div>
-      <div>
-        <h2 className={styles.title}>{t(isSignUp ? 'sign up' : 'sign in')}</h2>
+      <div className={styles.formCard}>
+        <div>
+          <img className={styles.welcomeImage} src={authGreeting} />
+          <p className={styles.imageTitle}>{t('image title')}</p>
+          <p className={styles.imageSubtitle}>{t('image subtitle')}</p>
+        </div>
+        <div>
+          <h2 className={styles.title}>{t(isSignUp ? 'sign up' : 'sign in')}</h2>
 
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
-          {isSignUp ? (
-            <>
-              <TextField
-                className={styles.nameInput}
-                id="name-input"
-                type="text"
-                placeholder={t('name') || ''}
-                helperText={errors.name?.message || ' '}
-                error={!!errors.name}
-                fullWidth
-                size="small"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Person />
-                    </InputAdornment>
-                  ),
-                }}
-                {...register('name', {
-                  required: t('this field is required') || '',
-                  minLength: {
-                    value: 2,
-                    message: t('minimum length 2 symbols') || '',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: t('maximum length 20 symbols') || '',
-                  },
-                  pattern: {
-                    value: /^[a-zA-Zа-яёА-ЯЁ]+$/u,
-                    message: t('must only contain letters') || '',
-                  },
-                })}
-              />
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+            {isSignUp ? (
+              <>
+                <TextField
+                  className={styles.nameInput}
+                  id="name-input"
+                  type="text"
+                  placeholder={t('name') || ''}
+                  helperText={errors.name?.message || ' '}
+                  error={!!errors.name}
+                  fullWidth
+                  size="small"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Person />
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...register('name', {
+                    required: t('this field is required') || '',
+                    minLength: {
+                      value: 2,
+                      message: t('minimum length 2 symbols') || '',
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: t('maximum length 20 symbols') || '',
+                    },
+                    pattern: {
+                      value: /^[a-zA-Zа-яёА-ЯЁ]+$/u,
+                      message: t('must only contain letters') || '',
+                    },
+                  })}
+                />
 
-              <TextField
-                className={styles.loginInput}
-                id="login-input"
-                type="text"
-                placeholder={t('login') || ''}
-                helperText={errors.login?.message || ' '}
-                error={!!errors.login}
-                fullWidth
-                size="small"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Login />
-                    </InputAdornment>
-                  ),
-                }}
-                {...register('login', {
-                  required: t('this field is required') || '',
-                  minLength: {
-                    value: 4,
-                    message: t('minimum length 4 symbols') || '',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: t('maximum length 20 symbols') || '',
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z1-9]+$/,
-                    message: t('must only contain latin letters or/and digits') || '',
-                  },
-                  validate: (value) =>
-                    !parseInt(value.slice(0, 1)) || t('can only start with a letter') || '',
-                })}
-              />
+                <TextField
+                  className={styles.loginInput}
+                  id="login-input"
+                  type="text"
+                  placeholder={t('login') || ''}
+                  helperText={errors.login?.message || ' '}
+                  error={!!errors.login}
+                  fullWidth
+                  size="small"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Login />
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...register('login', {
+                    required: t('this field is required') || '',
+                    minLength: {
+                      value: 4,
+                      message: t('minimum length 4 symbols') || '',
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: t('maximum length 20 symbols') || '',
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z1-9]+$/,
+                      message: t('must only contain latin letters or/and digits') || '',
+                    },
+                    validate: (value) =>
+                      !parseInt(value.slice(0, 1)) || t('can only start with a letter') || '',
+                  })}
+                />
 
-              <TextField
-                className={styles.passwordInput}
-                id="password-input"
-                type={isShowingPassword ? 'text' : 'password'}
-                placeholder={t('password') || ''}
-                helperText={errors.password?.message || ' '}
-                error={!!errors.password}
-                size="small"
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockIcon />
-                    </InputAdornment>
-                  ),
+                <TextField
+                  className={styles.passwordInput}
+                  id="password-input"
+                  type={isShowingPassword ? 'text' : 'password'}
+                  placeholder={t('password') || ''}
+                  helperText={errors.password?.message || ' '}
+                  error={!!errors.password}
+                  size="small"
+                  fullWidth
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockIcon />
+                      </InputAdornment>
+                    ),
 
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {isShowingPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                {...register('password', {
-                  required: t('this field is required') || '',
-                  minLength: {
-                    value: 8,
-                    message: t('minimum length 8 symbols') || '',
-                  },
-                  pattern: {
-                    value: /^(?=.*[0-9])(?=.*[a-zа-яё])(?=.*[A-ZА-ЯЁ])/,
-                    message:
-                      t('at least one number, one uppercase and one lowercase letters') || '',
-                  },
-                })}
-              />
-            </>
-          ) : (
-            <>
-              <TextField
-                className={styles.loginInput}
-                id="login-input"
-                type="text"
-                placeholder={t('login') || ''}
-                helperText={errors.login?.message || ' '}
-                error={!!errors.login}
-                fullWidth
-                size="small"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Login />
-                    </InputAdornment>
-                  ),
-                }}
-                {...register('login', {
-                  required: t('this field is required') || '',
-                })}
-              />
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {isShowingPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...register('password', {
+                    required: t('this field is required') || '',
+                    minLength: {
+                      value: 8,
+                      message: t('minimum length 8 symbols') || '',
+                    },
+                    pattern: {
+                      value: /^(?=.*[0-9])(?=.*[a-zа-яё])(?=.*[A-ZА-ЯЁ])/,
+                      message:
+                        t('at least one number, one uppercase and one lowercase letters') || '',
+                    },
+                  })}
+                />
+              </>
+            ) : (
+              <>
+                <TextField
+                  className={styles.loginInput}
+                  id="login-input"
+                  type="text"
+                  placeholder={t('login') || ''}
+                  helperText={errors.login?.message || ' '}
+                  error={!!errors.login}
+                  fullWidth
+                  size="small"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Login />
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...register('login', {
+                    required: t('this field is required') || '',
+                  })}
+                />
 
-              <TextField
-                className={styles.passwordInput}
-                id="password-input"
-                type={isShowingPassword ? 'text' : 'password'}
-                placeholder={t('password') || ''}
-                helperText={errors.password?.message || ' '}
-                error={!!errors.password}
-                size="small"
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockIcon />
-                    </InputAdornment>
-                  ),
+                <TextField
+                  className={styles.passwordInput}
+                  id="password-input"
+                  type={isShowingPassword ? 'text' : 'password'}
+                  placeholder={t('password') || ''}
+                  helperText={errors.password?.message || ' '}
+                  error={!!errors.password}
+                  size="small"
+                  fullWidth
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockIcon />
+                      </InputAdornment>
+                    ),
 
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {isShowingPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                {...register('password', {
-                  required: t('this field is required') || '',
-                })}
-              />
-            </>
-          )}
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {isShowingPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...register('password', {
+                    required: t('this field is required') || '',
+                  })}
+                />
+              </>
+            )}
 
-          <Button className={styles.submitButton} type="submit" variant="contained" fullWidth>
-            {t(isSignUp ? 'sign up' : 'sign in')}
-          </Button>
-        </form>
+            <Button className={styles.submitButton} type="submit" variant="contained" fullWidth>
+              {t(isSignUp ? 'sign up' : 'sign in')}
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
