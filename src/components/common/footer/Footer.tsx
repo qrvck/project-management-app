0d64fbc15264c5a2e40a646c396571de0e689bdb
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-20 13:57:10 +0300

    fix: update footer component

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 function Footer() {
   return (
     <footer>
-      <div className="container">
-        <h1>Footer</h1>
+      <div className={`container ${styles.container}`}>
+        <p className={styles.copyright}>Copyright | 2022</p>
+        <div className={styles.authors}>
+          <a className={styles.authorLink} href="https://github.com/qrvck">
+            qrvck
+          </a>
+          <a className={styles.authorLink} href="https://github.com/azozulya">
+            azozulya
+          </a>
+          <a className={styles.authorLink} href="https://github.com/kosirina">
+            kosirina
+          </a>
+        </div>
+        <a className={styles.courseLogo} href="https://rs.school/react/" />
       </div>
     </footer>
   );
