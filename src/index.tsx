23b7488cec4eeee9d94ce6d5b6f6a1aa408ba7f2
azozulya <bbbom@mail.ru>
2022-11-21 12:14:02 +0200

    fix: delete theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@emotion/react';
-
-import './i18n';
 import App from './App';
-import PMTheme from 'theme/theme';
+import { BrowserRouter } from 'react-router-dom';
+import './i18n';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={PMTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
