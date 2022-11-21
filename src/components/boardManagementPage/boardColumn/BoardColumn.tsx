128da2d3702d8341d463490087a1af8a737d230a
azozulya <bbbom@mail.ru>
2022-11-21 13:41:08 +0200

    refactor: create board component

---
+++ src/components/boardManagementPage/boardColumn/BoardColumn.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { grey } from '@mui/material/colors';
+import { Box, Button, Grid, Paper } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { TaskList, TTask } from '../taskList';
+import styles from './BoardColumn.module.scss';
+
+type TBoardColumnProps = {
+  id: string;
+  label: string;
+  items: TTask[];
+};
+
+function BoardColumn({ id, label, items }: TBoardColumnProps) {
+  const { t } = useTranslation('board-management-page');
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id,
+    data: {
+      type: 'column',
+      columnId: 'root',
+    },
+  });
+
+  return (
+    <Grid item xs={4}>
+      <Paper
+        elevation={3}
+        ref={setNodeRef}
+        className={styles.column}
+        sx={{
+          transform: CSS.Transform.toString(transform),
+          transition,
+          cursor: isDragging ? 'grabbing' : 'grab',
+          backgroundColor: isDragging ? grey[300] : grey[200],
+        }}
+        {...listeners}
+        {...attributes}
+      >
+        <h3 className={styles.title}>{label}</h3>
+
+        <TaskList items={items} columnId={id} />
+        <Box p={1}>
+          <Button size="small" color="secondary" variant="contained">
+            + {t('addTask')}
+          </Button>
+        </Box>
+      </Paper>
+    </Grid>
+  );
+}
+
+export default BoardColumn;
