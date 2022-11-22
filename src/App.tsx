be0a09a9bb6ef5dc7cc1090372cc6cd283b27628
qrvck <v.kapitan2010@gmail.com>
2022-11-22 04:03:18 +0300

    feat: create sign in and sign up pages and add routes to them

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from 'components/common/layout/Layout';
-import { AuthPage, BoardManagementPage, BoardsListPage, WelcomePage, EditProfilePage } from 'pages';
+import {
+  SignUpPage,
+  SignInPage,
+  BoardManagementPage,
+  BoardsListPage,
+  WelcomePage,
+  EditProfilePage,
+} from 'pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<WelcomePage />} />
-        <Route path="sign-in" element={<AuthPage />} />
-        <Route path="sign-up" element={<AuthPage />} />
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route path="sign-up" element={<SignUpPage />} />
         <Route path="edit-profile" element={<EditProfilePage />} />
         <Route path="board-management" element={<BoardManagementPage />} />
         <Route path="boards-list" element={<BoardsListPage />} />
