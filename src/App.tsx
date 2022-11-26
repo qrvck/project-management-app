cd1acab176319e48e3b07751513131b942a4beae
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-26 13:51:51 +0300

    feat: wrap routes with error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import ErrorBoundary from 'components/errorBoundary/ErrorBoundary';
 
 import Layout from 'components/common/layout/Layout';
 import {
@@ -13,18 +14,20 @@ import {
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<WelcomePage />} />
-        <Route path="sign-in" element={<AuthPage />} />
-        <Route path="sign-up" element={<AuthPage />} />
-        <Route path="edit-profile" element={<EditProfilePage />} />
-        <Route path="board-management" element={<BoardManagementPage />} />
-        <Route path="boards-list" element={<BoardsListPage />} />
-        <Route path="404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="404" />} />
-      </Route>
-    </Routes>
+    <ErrorBoundary useSuspense={false} key={useLocation().pathname}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="sign-in" element={<AuthPage />} />
+          <Route path="sign-up" element={<AuthPage />} />
+          <Route path="edit-profile" element={<EditProfilePage />} />
+          <Route path="board-management" element={<BoardManagementPage />} />
+          <Route path="boards-list" element={<BoardsListPage />} />
+          <Route path="404" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
