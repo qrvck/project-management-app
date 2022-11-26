baedd1eb72f088eb36d8776789776b151b5f336b
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-26 15:25:15 +0300

    feat: make footer links open in new window

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -7,17 +7,37 @@ function Footer() {
       <div className={`container ${styles.container}`}>
         <p className={styles.copyright}>Copyright | 2022</p>
         <div className={styles.authors}>
-          <a className={styles.authorLink} href="https://github.com/qrvck">
+          <a
+            className={styles.authorLink}
+            href="https://github.com/qrvck"
+            target="_blank"
+            rel="noreferrer"
+          >
             qrvck
           </a>
-          <a className={styles.authorLink} href="https://github.com/azozulya">
+          <a
+            className={styles.authorLink}
+            href="https://github.com/azozulya"
+            target="_blank"
+            rel="noreferrer"
+          >
             azozulya
           </a>
-          <a className={styles.authorLink} href="https://github.com/kosirina">
+          <a
+            className={styles.authorLink}
+            href="https://github.com/kosirina"
+            target="_blank"
+            rel="noreferrer"
+          >
             kosirina
           </a>
         </div>
-        <a className={styles.courseLogo} href="https://rs.school/react/" />
+        <a
+          className={styles.courseLogo}
+          href="https://rs.school/react/"
+          target="_blank"
+          rel="noreferrer"
+        />
       </div>
     </footer>
   );
