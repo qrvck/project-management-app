65e568bea7f1741b9b17d0d48ecf29e0fafb4f2e
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-11-26 10:33:56 +0300

    feat: implement suspense and lazy loading for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-
 import Layout from 'components/common/layout/Layout';
-import {
-  AuthPage,
-  BoardManagementPage,
-  BoardsListPage,
-  WelcomePage,
-  EditProfilePage,
-  NotFoundPage,
-} from 'pages';
+import Loader from 'components/loader';
+
+const AuthPage = lazy(() => import('pages/AuthPage'));
+const BoardManagementPage = lazy(() => import('pages/BoardManagementPage'));
+const BoardsListPage = lazy(() => import('pages/BoardsListPage'));
+const WelcomePage = lazy(() => import('pages/WelcomePage'));
+const EditProfilePage = lazy(() => import('pages/EditProfilePage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<WelcomePage />} />
-        <Route path="sign-in" element={<AuthPage />} />
-        <Route path="sign-up" element={<AuthPage />} />
-        <Route path="edit-profile" element={<EditProfilePage />} />
-        <Route path="board-management" element={<BoardManagementPage />} />
-        <Route path="boards-list" element={<BoardsListPage />} />
-        <Route path="404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="404" />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="sign-in" element={<AuthPage />} />
+          <Route path="sign-up" element={<AuthPage />} />
+          <Route path="edit-profile" element={<EditProfilePage />} />
+          <Route path="board-management" element={<BoardManagementPage />} />
+          <Route path="boards-list" element={<BoardsListPage />} />
+          <Route path="404" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
