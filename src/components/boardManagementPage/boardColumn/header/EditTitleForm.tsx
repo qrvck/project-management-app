fccc035013fd7c0e9e42c48cdf32026d1e97eeae
azozulya <bbbom@mail.ru>
2022-11-29 12:07:52 +0200

    feat: add snack message after edit title

--- src/components/boardManagementPage/boardColumn/header/EditTitleForm.tsx
+++ src/components/boardManagementPage/boardColumn/header/EditTitleForm.tsx
@@ -1,31 +1,52 @@
 import React from 'react';
 import Input from '@mui/material/Input';
-
 import styles from './EditTitleForm.module.scss';
+import { TSnackbarMessage } from 'components/common/snackbar';
 
 type TEditTitleForm = {
   label: string;
-  onClickAway: () => void;
+  close: () => void;
+  showSnackMessage: (props: TSnackbarMessage) => void;
 };
 
-function EditTitleForm({ label, onClickAway }: TEditTitleForm) {
+function EditTitleForm({ label, close, showSnackMessage }: TEditTitleForm) {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    onClickAway();
+
+    showSnackMessage({
+      isOpen: true,
+      severity: 'success',
+      message: 'Title has been updated',
+    });
+
+    close();
+  };
+
+  const handlerClose = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    showSnackMessage({
+      isOpen: true,
+      severity: 'error',
+      message: 'Title has not been updated',
+    });
+
+    close();
   };
 
   return (
-    <form autoComplete="off" className={styles.editForm} onSubmit={handleSubmit}>
-      <Input
-        className={styles.input}
-        defaultValue={label}
-        autoFocus={true}
-        inputProps={{ 'aria-label': 'description' }}
-        fullWidth
-        disableUnderline
-        onBlur={onClickAway}
-      />
-    </form>
+    <>
+      <form autoComplete="off" className={styles.editForm} onSubmit={handleSubmit}>
+        <Input
+          className={styles.input}
+          defaultValue={label}
+          autoFocus={true}
+          fullWidth
+          disableUnderline
+          onBlur={handlerClose}
+        />
+      </form>
+    </>
   );
 }
 
