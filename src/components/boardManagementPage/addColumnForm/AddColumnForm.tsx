86c8835572950548bc83496426f4a161de2e18f5
azozulya <bbbom@mail.ru>
2022-11-30 22:19:31 +0200

    refactor: use the standard dialog to create the form

--- src/components/boardManagementPage/addColumnForm/AddColumnForm.tsx
+++ src/components/boardManagementPage/addColumnForm/AddColumnForm.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { ModalForm } from '../../common/modalForm';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import { TAddColumnFormProps, TAddColumnFormValues } from './AddColumn.types';
 import { SubmitHandler, useForm } from 'react-hook-form';
+
 import styles from './AddColumnForm.module.scss';
 // import { useTranslation } from 'react-i18next';
 
@@ -27,36 +29,38 @@ export default function AddColumnForm({ isOpen, onSubmit, onClose }: TAddColumnF
   };
 
   return (
-    <ModalForm isOpen={isOpen} onClose={onClose} title="Create column">
-      <form onSubmit={handleSubmit(handlerSubmit)} noValidate>
-        <TextField
-          focused
-          autoComplete="off"
-          autoFocus
-          margin="dense"
-          label="Column name"
-          type="text"
-          fullWidth
-          variant="outlined"
-          helperText="Column name must be at least 2 symbols and maximum 50"
-          {...register('columnName', {
-            required: 'This field is required',
-            minLength: 2,
-            maxLength: 50,
-          })}
-          error={!!errors.columnName}
-        />
-
-        <div className={styles.buttons}>
-          <Button type="submit" variant="contained">
-            {/* {t('addButton')} */} add column
-          </Button>
-          <Button onClick={onClose}>
-            {/* {t('cancelButton')} */}
-            cancel
-          </Button>
-        </div>
-      </form>
-    </ModalForm>
+    <Dialog open={isOpen} onClose={onClose} maxWidth="xs" fullWidth>
+      <h3 className={styles.title}>Create column</h3>
+      <DialogContent>
+        <form onSubmit={handleSubmit(handlerSubmit)} noValidate>
+          <TextField
+            focused
+            autoComplete="off"
+            autoFocus
+            margin="dense"
+            label="Column name"
+            type="text"
+            fullWidth
+            variant="outlined"
+            helperText="Column name must be at least 2 symbols and maximum 50"
+            {...register('columnName', {
+              required: 'This field is required',
+              minLength: 2,
+              maxLength: 50,
+            })}
+            error={!!errors.columnName}
+          />
+          <div className={styles.buttons}>
+            <Button type="submit" variant="contained">
+              {/* {t('addButton')} */} add column
+            </Button>
+            <Button onClick={onClose}>
+              {/* {t('cancelButton')} */}
+              cancel
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
