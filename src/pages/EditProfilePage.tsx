ac104aa49eb4f43afc9c6d640939dd9554acf7b1
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-12-01 13:20:55 +0300

    feat: implement user form in edit profile page

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { SubmitHandler } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import UserForm from 'components/common/userForm/UserForm';
+import { TFormValues } from 'components/common/userForm/UserForm.types';
 import styles from './EditProfilePage.module.scss';
 
 function EditProfilePage() {
   const { t } = useTranslation('edit-profile-page');
+
+  const onSubmit: SubmitHandler<TFormValues> = ({ name, login, password }) => {
+    //добавить функционал
+  };
 
   return (
     <div className={`container ${styles.container}`}>
@@ -13,6 +20,9 @@ function EditProfilePage() {
       <div className={styles.warning}>
         <PriorityHighIcon className={styles.warningIcon} />
         <span>{t('warning')}</span>
+      </div>
+      <div className={styles.formContainer}>
+        <UserForm submitButton={t('editButton')} submitCallback={onSubmit} />
       </div>
       <Button className={styles.deleteButton} variant="contained">
         {t('deleteButton')}
