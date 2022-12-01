7196dd4cc7e177ad7519f281afd55b4be8a1fb07
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-12-01 10:40:55 +0300

    feat: add main elements to edit profile page and style them

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from '@mui/material/Button';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import styles from './EditProfilePage.module.scss';
 
 function EditProfilePage() {
-  return <h1>Edit Profile Page</h1>;
+  const { t } = useTranslation('edit-profile-page');
+
+  return (
+    <div className={`container ${styles.container}`}>
+      <h2 className={styles.pageTitle}>{t('title')}</h2>
+      <div className={styles.warning}>
+        <PriorityHighIcon className={styles.warningIcon} />
+        <span>{t('warning')}</span>
+      </div>
+      <Button className={styles.deleteButton} variant="contained">
+        {t('deleteButton')}
+      </Button>
+    </div>
+  );
 }
 
 export default EditProfilePage;
