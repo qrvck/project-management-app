089362e579b25be3e64387b3f24423f221771079
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-12-03 05:21:46 +0300

    fix: change way to pass translation values for validation errors in sign and user forms

--- src/components/common/userForm/UserForm.tsx
+++ src/components/common/userForm/UserForm.tsx
@@ -47,7 +47,14 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
           id="name-input"
           type="text"
           placeholder={t('name') || ''}
-          helperText={errors.name?.message || ' '}
+          helperText={
+            errors.name?.message
+              ? t(
+                  `${JSON.parse(errors.name.message).key as string}`,
+                  JSON.parse(errors.name.message).param as { [key: string]: string } | undefined
+                )
+              : ' '
+          }
           error={!!errors.name}
           fullWidth
           size="small"
@@ -59,18 +66,20 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
             ),
           }}
           {...register('name', {
-            required: t('this field is required') || '',
+            required: JSON.stringify({ key: 'this field is required' }) || '',
             minLength: {
               value: 2,
-              message: t('minimum length symbols', { symbolsCount: '2' }) || '',
+              message:
+                JSON.stringify({ key: 'minimum length symbols', param: { symbolsCount: '2' } }) ||
+                '',
             },
             maxLength: {
               value: 20,
-              message: t('maximum length 20 symbols') || '',
+              message: JSON.stringify({ key: 'maximum length 20 symbols' }) || '',
             },
             pattern: {
               value: /^[a-zA-Zа-яёА-ЯЁ]+$/u,
-              message: t('must contain only letters') || '',
+              message: JSON.stringify({ key: 'must contain only letters' }) || '',
             },
           })}
         />
@@ -80,7 +89,14 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
           id="login-input"
           type="text"
           placeholder={t('login') || ''}
-          helperText={errors.login?.message || ' '}
+          helperText={
+            errors.login?.message
+              ? t(
+                  `${JSON.parse(errors.login.message).key as string}`,
+                  JSON.parse(errors.login.message).param as { [key: string]: string } | undefined
+                )
+              : ' '
+          }
           error={!!errors.login}
           fullWidth
           size="small"
@@ -92,21 +108,26 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
             ),
           }}
           {...register('login', {
-            required: t('this field is required') || '',
+            required: JSON.stringify({ key: 'this field is required' }) || '',
             minLength: {
               value: 4,
-              message: t('minimum length symbols', { symbolsCount: '4' }) || '',
+              message:
+                JSON.stringify({ key: 'minimum length symbols', param: { symbolsCount: '4' } }) ||
+                '',
             },
             maxLength: {
               value: 20,
-              message: t('maximum length 20 symbols') || '',
+              message: JSON.stringify({ key: 'maximum length 20 symbols' }) || '',
             },
             pattern: {
               value: /^[a-zA-Z1-9]+$/,
-              message: t('must contain only latin letters or/and digits') || '',
+              message:
+                JSON.stringify({ key: 'must contain only latin letters or/and digits' }) || '',
             },
             validate: (inputValue) =>
-              !parseInt(inputValue[0]) || t('must start only with a letter') || '',
+              !parseInt(inputValue[0]) ||
+              JSON.stringify({ key: 'must start only with a letter' }) ||
+              '',
           })}
         />
 
@@ -115,7 +136,14 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
           id="password-input"
           type={isShowingPassword ? 'text' : 'password'}
           placeholder={t('password') || ''}
-          helperText={errors.password?.message || ' '}
+          helperText={
+            errors.password?.message
+              ? t(
+                  `${JSON.parse(errors.password.message).key as string}`,
+                  JSON.parse(errors.password.message).param as { [key: string]: string } | undefined
+                )
+              : ' '
+          }
           error={!!errors.password}
           size="small"
           fullWidth
@@ -140,14 +168,19 @@ function UserForm({ submitButton, onSubmit }: IUserFormProps) {
             ),
           }}
           {...register('password', {
-            required: t('this field is required') || '',
+            required: JSON.stringify({ key: 'this field is required' }) || '',
             minLength: {
               value: 8,
-              message: t('minimum length symbols', { symbolsCount: '8' }) || '',
+              message:
+                JSON.stringify({ key: 'minimum length symbols', param: { symbolsCount: '8' } }) ||
+                '',
             },
             pattern: {
               value: /^(?=.*[0-9])(?=.*[a-zа-яё])(?=.*[A-ZА-ЯЁ])/,
-              message: t('at least one number, one uppercase and one lowercase letters') || '',
+              message:
+                JSON.stringify({
+                  key: 'at least one number, one uppercase and one lowercase letters',
+                }) || '',
             },
           })}
         />
