45326076b5cb7f6f552a047593ae2c1bcf452c4d
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-12-04 10:33:19 +0300

    feat: implement getting and displaying boards from api

--- src/components/boardsListPage/boardsList/BoardsList.tsx
+++ src/components/boardsListPage/boardsList/BoardsList.tsx
@@ -1,48 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import BoardCard from '../boardCard';
+import Loader from 'components/common/loader';
+import { getAllBoardsCall } from 'api/boards';
+import { TBoards } from 'api/types';
+import useAuth from 'auth/useAuth';
+import { useTranslation } from 'react-i18next';
 import styles from './BoardsList.module.scss';
 
-const boards = [
-  {
-    _id: '1',
-    title: 'rss Lang',
-    owner: 'Vlad',
-  },
-  {
-    _id: '2',
-    title: 'async race',
-    owner: 'Vlad',
-  },
-  {
-    _id: '3',
-    title: 'Online-Store',
-    owner: 'Vlad',
-  },
-
-  {
-    _id: '4',
-    title: 'virtual keyboard',
-    owner: 'Ira',
-  },
-  {
-    _id: '5',
-    title: 'shelter',
-    owner: 'Anna',
-  },
-  {
-    _id: '6',
-    title: 'CSS Mem Slider',
-    owner: 'Aliaksandr',
-  },
-];
-
 function BoardsList() {
+  const { t } = useTranslation('boards-list-page');
+  const { user } = useAuth();
+  const [boards, setBoards] = useState<TBoards>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const getAllBoards = useCallback(async (): Promise<void> => {
+    setIsLoading(true);
+    setErrorMessage(null);
+    try {
+      const boards: TBoards = await getAllBoardsCall(user.token);
+      setBoards(boards);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage('unknownError');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, [user.token]);
+
+  useEffect(() => {
+    getAllBoards();
+  }, [getAllBoards]);
+
   return (
-    <div className={styles.list}>
-      {boards.map((board) => (
-        <BoardCard key={board._id} to={board._id} title={board.title} owner={board.owner} />
-      ))}
-    </div>
+    <>
+      <div className={styles.list}>
+        {isLoading && <Loader />}
+        {!!boards.length &&
+          !isLoading &&
+          !errorMessage &&
+          boards.map((board) => (
+            <BoardCard key={board._id} to={board._id} title={board.title} owner={board.owner} />
+          ))}
+      </div>
+      {errorMessage && <p>{t(`${errorMessage}`)}</p>}
+      {!boards.length && !isLoading && !errorMessage && <p>{t('noBoards')}</p>}
+    </>
   );
 }
 
