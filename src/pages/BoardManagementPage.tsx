f7bfd0e7b8ef44b61a7c89d1e50190bb86ab93e3
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-12-04 14:39:27 +0300

    feat: add back button to board page, styles and some translation

--- src/pages/BoardManagementPage.tsx
+++ src/pages/BoardManagementPage.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Board from 'components/boardManagementPage';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import styles from './BoardManagementPage.module.scss';
 
-type TBoardManagementPageProps = {
-  boardId?: number;
-};
-
-function BoardManagementPage({ boardId = 1 }: TBoardManagementPageProps) {
+function BoardManagementPage() {
   const { t } = useTranslation('board-management-page');
+  const { boardId } = useParams();
 
   return (
     <div className={`container ${styles.wrapper}`}>
-      <h2>
+      <h2 className={styles.title}>
         {t('title')} [boardId={boardId}]
       </h2>
-      <Box mb={2}>
-        <Button variant="contained">+ {t('addColumn')}</Button>
+
+      <Box className={styles.buttons} mb={2}>
+        <Button
+          className={styles.redirectButton}
+          variant="outlined"
+          component={Link}
+          to="/boards-list"
+        >
+          {t('backButton')}
+        </Button>
+        <Button className={styles.addButton} type="submit" variant="contained">
+          + {t('addColumn')}
+        </Button>
       </Box>
       <Board />
     </div>
